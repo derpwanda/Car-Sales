c932e0112a2dfb3d8f9d384c464f5d3db168ccf9
derpWanda <derpWanda@gmail.com>
2019-10-23 09:59:22 -0500

    added reduxdevtools code to store

--- src/index.js
+++ src/index.js
@@ -6,7 +6,14 @@ import App from './App';
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
-const store = createStore();
+const tempReducer = (() => {
+    return {
+        title: "Hello from the Redux Store!"
+    }
+})
+
+const store = createStore(tempReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 const rootElement = document.getElementById('root');
 
 ReactDOM.render(<App />, rootElement);
