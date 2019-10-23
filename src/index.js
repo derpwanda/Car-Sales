e2ccb3b432ca83a491173b0b6b63bd5a4ccffb2c
derpWanda <derpWanda@gmail.com>
2019-10-23 13:00:21 -0500

    moved data from App to reducers; import to index

--- src/index.js
+++ src/index.js
@@ -3,17 +3,13 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import App from './App';
+import { reducer } from './reducers/reducers.js'
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
-const tempReducer = (() => {
-    return {
-        title: "Hello from the Redux Store!"
-    }
-})
 
-const store = createStore(tempReducer,
+const store = createStore(reducer,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 const rootElement = document.getElementById('root');
 
