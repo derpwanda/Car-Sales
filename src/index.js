d58b3c5da2e52d26879483e60106a65702a18e9c
derpWanda <derpWanda@gmail.com>
2019-10-23 09:42:43 -0500

    added dependencies; setup redux store

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 import App from './App';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+const store = createStore();
 const rootElement = document.getElementById('root');
+
 ReactDOM.render(<App />, rootElement);
