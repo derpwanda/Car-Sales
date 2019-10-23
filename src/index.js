3394dace171bb474f540a83d3f6554ea1e8d0e3e
derpWanda <derpWanda@gmail.com>
2019-10-23 10:10:52 -0500

    added Provider, wrapped App

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from './App';
 
 import 'bulma/css/bulma.css';
@@ -16,4 +17,4 @@ const store = createStore(tempReducer,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 const rootElement = document.getElementById('root');
 
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Provider store={store}><App /></Provider>, rootElement);
